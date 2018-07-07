afb0f5b9c3fb61166233061edb55aad3e57a101d
Kritika Ganapathy <ganapathy.kritika@gmail.com>
2018-07-07 15:33:35 -0400

    removed example LCBO API call

--- src/App.js
+++ src/App.js
@@ -1,22 +1,14 @@
 import React, { Component } from "react";
 
-import { fetchLcboEndpoint } from "./api/lcbo.js";
+import Searchbar from './Searchbar.js'
 
 class App extends Component {
-  componentDidMount() {
-    // example of making an API request to the LCBO API
-    fetchLcboEndpoint("products", {
-      q: "radler"
-    }).then(data => {
-      console.log(data);
-    });
-  }
+
   render() {
     return (
       <div>
-        <p>
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Searchbar />
+
       </div>
     );
   }
